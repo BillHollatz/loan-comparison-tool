1d0528c2c9d0f9e8e8290a053580ae5102ec55cb
BillHollatz <83040621+BillHollatz@users.noreply.github.com>
2021-10-06 21:09:21 -0500

    minor fixes to classloan

--- pages/api/LoanInputs.js
+++ pages/api/LoanInputs.js
@@ -3,10 +3,11 @@ import Loan from './backend/classloan'
 
 
 export default function handler(req, res) {
-	const Key = req.body.Key
-	const Amount = req.body.Amount
-	const Rate = req.body.Rate
-	const Term = req.body.Term
+	const Key = Number(req.body.Key)
+	const Amount = Number(req.body.Amount)
+	const Rate = Number(req.body.Rate)
+	const Term = Number(req.body.Term)
 	let x = new Loan(Amount, Rate, Term)
+	let y =  x.getAmmount()
 	res.status(200).json({Key, x})
 }
--- pages/api/backend/classloan.js
+++ pages/api/backend/classloan.js
@@ -1,11 +1,13 @@
 import MonthList from './classmonthlist'
 class Loan {
 constructor(Ammount, Rate, Term) {
-this.Ammount = Ammount;
-this.Rate = Rate;
-this.Term = Term;
-let C = this.rate/1200;
-	this.MonthlyPayment =(this.Ammount*(C*(Math.pow(1+C,360))))/((Math.pow(1+C,360))-1);
+	this.Ammount = Ammount;
+	this.Rate = Rate;
+	this.Term = Term;
+	
+	let C = Rate/1200;
+	this.x = C;
+	this.MonthlyPayment =(Ammount*(C*(Math.pow(1+C,360))))/((Math.pow(1+C,360))-1);
 	this.Months = new MonthList(this.Ammount,this.MonthlyPayment,C)
 }
 getMonthlyPayment(){
