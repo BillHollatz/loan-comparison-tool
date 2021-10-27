0cb8018ba0f6de434cc997a1ccbfb69d91c9d899
BillHollatz <83040621+BillHollatz@users.noreply.github.com>
2021-10-27 15:51:20 -0500

    Update auth.tsx

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -3,13 +3,16 @@ import React from "react";
 import StyledFirebaseAuth from "react-firebaseui/StyledFirebaseAuth";
 import app from "../firebase/clientApp";
 
+
+const auth = getAuth(app)
 // Configure FirebaseUI.
 const uiConfig = {
   // Redirect to / after sign in is successful. Alternatively you can provide a callbacks.signInSuccess function.
   signInSuccessUrl: "/",
   // GitHub as the only included Auth Provider.
   // You could add and configure more here!
-  signInOptions: [app.auth.GithubAuthProvider.PROVIDER_ID],
+  
+  signInOptions: [auth.GithubAuthProvider.PROVIDER_ID],
 };
 
 function SignInScreen() {
