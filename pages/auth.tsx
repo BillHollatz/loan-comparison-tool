9ff98357d8b80f283b701a9e395c8500054ab1a6
BillHollatz <bill@billhollatz.com>
2021-10-27 16:16:33 -0500

    Update auth.tsx

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -1,4 +1,6 @@
 // auth.tsx
+import {getAuth} from "firebase/auth";
+
 import React from "react";
 import StyledFirebaseAuth from "react-firebaseui/StyledFirebaseAuth";
 import app from "../firebase/clientApp";
