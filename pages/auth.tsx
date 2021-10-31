26fe708af564f52630cbc154ac2d90ec2e04316a
BillHollatz <83040621+BillHollatz@users.noreply.github.com>
2021-10-31 13:21:02 -0500

    Update auth.tsx

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -7,9 +7,78 @@ import app from "../firebase/clientApp";
 
 
 const auth = getAuth(app)
+
+createUserWithEmailAndPassword(auth, email, password)
+  .then((userCredential) => {
+    // Signed in 
+    const user = userCredential.user;
+    // ...
+  })
+  .catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    // ..
+  });
+  
+  signInWithEmailAndPassword(auth, email, password)
+  .then((userCredential) => {
+    // Signed in 
+    const user = userCredential.user;
+    // ...
+  })
+  .catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+  });
+
+
+onAuthStateChanged(auth, (user) => {
+  if (user) {
+    // User is signed in, see docs for a list of available properties
+    // https://firebase.google.com/docs/reference/js/firebase.User
+    const uid = user.uid;
+    // ...
+  } else {
+    // User is signed out
+    // ...
+  }
+});
+
+
+var ui = new firebaseui.auth.AuthUI(firebase.auth());
+var uiConfig = {
+  callbacks: {
+    signInSuccessWithAuthResult: function(authResult, redirectUrl) {
+      // User successfully signed in.
+      // Return type determines whether we continue the redirect automatically
+      // or whether we leave that to developer to handle.
+      return true;
+    },
+    uiShown: function() {
+      // The widget is rendered.
+      // Hide the loader.
+      document.getElementById('loader').style.display = 'none';
+    }
+  },
+  // Will use popup for IDP Providers sign-in flow instead of the default, redirect.
+  signInFlow: 'popup',
+  signInSuccessUrl: '<url-to-redirect-to-on-success>',
+  signInOptions: [
+    // Leave the lines as is for the providers you want to offer your users.
+    
+    firebase.auth.EmailAuthProvider.PROVIDER_ID
+   
+  ],
+  // Terms of service url.
+  tosUrl: '<your-tos-url>',
+  // Privacy policy url.
+  privacyPolicyUrl: '<your-privacy-policy-url>'
+};
+ui.start('#firebaseui-auth-container',uiConfig);
+/*
 var firebase = require('firebase');
 var firebaseui = require('firebaseui');
-var ui = new firebaseui.auth.AuthUI(firebase.auth());
+
 ui.start('#firebaseui-auth-container',{
   callbacks: {
     signInSuccessWithAuthResult: function(authResult, redirectUrl) {
@@ -30,7 +99,7 @@ ui.start('#firebaseui-auth-container',{
   
   signInOptions: [firebase.auth.EmailAuthProvider.PROVIDER_ID],
 });
-
+*/
 function SignInScreen() {
   return (
     <div>
