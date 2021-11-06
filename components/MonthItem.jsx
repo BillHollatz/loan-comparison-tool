01d5fced845d849f66e36ea615c6ad75a919e5a6
dkeita25 <90985816+dkeita25@users.noreply.github.com>
2021-11-06 10:52:57 -0500

    update
    update

--- components/MonthItem.jsx
+++ components/MonthItem.jsx
@@ -5,11 +5,21 @@ class ModificationItem extends Component{
 	constructor(props){
 		super(props)
 		this.Start = props.Start
+		this.SP = props.SP
+		this.MP = props.MP
+		this.MI = pros.MI
+		this.MPR = props.MPR
+		this.End = props.End
 	}
 	render() {
 		return (
 			<>
 				<p>{this.Start}</p>
+				<p>{this.SP}</P>
+				<p>{this.MP}</p>
+				<p>{this.MI}</p>
+				<p>{this.MPR}</p>
+				<p>{this.End}</p>
 			</>
 		)
 	}
